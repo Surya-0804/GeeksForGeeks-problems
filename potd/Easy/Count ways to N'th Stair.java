dfce61a521832014d99da8ed80c8b70295f76f4d
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-04-06 13:22:36 +0530

    Create Count ways to N'th Stair.java

---
+++ potd/Easy/Count ways to N'th Stair.java
@@ -0,0 +1,8 @@
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        // your code here
+        return (long)(n/2)+1;
+    }
+}
