288f573af1f72eb47eaead587a3fa38996166ace
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-05-01 19:25:23 +0530

    Create Learn to Comment - Java.java

---
+++ JavaBasic/Learn to Comment - Java.java
@@ -0,0 +1,15 @@
+class Geeks{
+    
+    // Function to print a and c
+    //comment the line that outputs b
+    static void comment(int a, int b, int c){
+        
+        //Prints a
+        System.out.println(a);
+        //Prints b
+        // System.out.println(b);
+        //Prints c
+        System.out.println(c);
+    }
+    
+}
