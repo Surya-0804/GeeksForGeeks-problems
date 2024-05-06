70532fffa820df0704b105b9d315a92ce2efabac
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-05-06 19:24:49 +0530

    Create Data Types - Java.java

---
+++ potd/Easy/Data Types - Java.java
@@ -0,0 +1,16 @@
+class Geeks{
+    
+    // Function to do operations with different data types
+    static void dataTypes(int a, float b, double c, long l, byte d){
+        
+        double p = c/b;//c/b
+        double q = b/a;//b/a
+        double r = c/a;//c/a
+        double m = r+l;//r+l
+        int s = a/d;//a/d
+        
+        //Printing all the results 
+        System.out.println(p + " " + q + " " + r + " " + m + " " + s);
+        
+    }
+}
