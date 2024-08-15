0a50d418c151ec6679418681c3e3574eac9397ad
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-08-15 20:03:24 +0530

    Create nextLine and next.java

---
+++ JavaBasic/Easy/nextLine and next.java
@@ -0,0 +1,21 @@
+//Complete the function
+//Take input for a and s 
+class Geeks {
+	static void getInput () {
+		
+	    Scanner sc = new Scanner(System.in);
+	    int t=sc.nextInt(); //Taking the number of testcases
+	    while(t-->0)
+	    {
+    		int a = sc.nextInt(); 
+    	    String s =sc.nextLine();
+    	    s+=sc.nextLine();
+    	    
+    	    //Your code here
+    
+    		System.out.println(a);
+    		System.out.println(s);
+	    }
+	
+	}
+}
