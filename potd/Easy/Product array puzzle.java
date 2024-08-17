b2732a7af05823b1f65bb14e307d18cd8dbc41a5
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-08-17 11:46:01 +0530

    Create Product array puzzle.java

---
+++ potd/Easy/Product array puzzle.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        // code here
+        int n=nums.length;
+        long[] result=new long[n];
+        result[0]=1;
+        for(int i=1; i<n; i++){
+            result[i]=result[i-1]*nums[i-1];
+        }
+        long x=1;
+        for(int i=n-1; i>=0; i--){
+            result[i]*=x;
+            x*=nums[i];
+        }
+        return result;
+    }
+}
