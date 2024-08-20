a237a1166337c874d176018f1bc2bc635da4b60e
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-08-20 19:03:30 +0530

    Create Bubble Sort (20-08-24).java

---
+++ Array/Easy/Bubble Sort (20-08-24).java
@@ -0,0 +1,20 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
