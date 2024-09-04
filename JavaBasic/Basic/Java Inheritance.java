9dd4b1d5c8534b58d2e22d174aee77ace1a81c12
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-09-04 14:29:37 +0530

    Create Java Inheritance.java

---
+++ JavaBasic/Basic/Java Inheritance.java
@@ -0,0 +1,12 @@
+class cls2 extends cls1
+{
+    void mul(int p,int q)
+    {
+        System.out.println(p*q);
+    }
+    void task(int p,int q)
+    {
+        //Add your code here.
+        System.out.println(p*p+q*q);
+    }
+}
