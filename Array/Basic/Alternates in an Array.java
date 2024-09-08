1fdb9889c79dd761d4e7257d9f01ad9b24de3b1b
Ram Sai Sri Surya Abothula <159000250+Surya-0804@users.noreply.github.com>
2024-09-08 22:37:44 +0530

    Create Alternates in an Array.java

---
+++ Array/Basic/Alternates in an Array.java
@@ -0,0 +1,11 @@
+
+// arr[] is the array
+// n is the number of elements in array.
+class Solution {
+    public static void print(int arr[]) {
+        // your code here
+        for(int i=0; i<arr.length; i+=2){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
